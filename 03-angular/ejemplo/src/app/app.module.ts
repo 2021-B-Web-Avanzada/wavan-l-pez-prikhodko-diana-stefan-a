d68806fa3f40d7d829c66e17ef8a3d1d97fccd5e
Diana <diana.lopez03@epn.edu.ec>
2022-01-28 17:26:33 -0500

    Add: Clase 29, 30 y 31

--- 03-angular/ejemplo/src/app/app.module.ts
+++ 03-angular/ejemplo/src/app/app.module.ts
@@ -18,6 +18,13 @@ import {BannerImagenesModule} from "./componentes/banner-imagenes/banner-imagene
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { RutaUsuarioPerfilComponent } from './rutas/ruta-usuario-perfil/ruta-usuario-perfil.component';
+import {InputSwitchModule} from "primeng/inputswitch";
+import {KnobModule} from "primeng/knob";
+import {SplitButtonModule} from "primeng/splitbutton";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDialogModule} from "@angular/material/dialog";
+import { ModalEjemploComponent } from './componentes/modales/modal-ejemplo/modal-ejemplo.component';
 
 @NgModule({
   //Componentes importados
@@ -30,7 +37,8 @@ import { RutaUsuarioPerfilComponent } from './rutas/ruta-usuario-perfil/ruta-usu
     RutaUsuarioComponent,
     RutaPostComponent,
     RutaAppComponent,
-    RutaUsuarioPerfilComponent
+    RutaUsuarioPerfilComponent,
+    ModalEjemploComponent
   ],
   //Módulos importados
   imports: [
@@ -39,7 +47,13 @@ import { RutaUsuarioPerfilComponent } from './rutas/ruta-usuario-perfil/ruta-usu
     BannerImagenesModule,
     HttpClientModule,
     FormsModule, // Template Driven Forms
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    InputSwitchModule,
+    KnobModule,
+    SplitButtonModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDialogModule,
   ],
   //Servicios importados
   providers: [
--- 03-angular/ejemplo/src/app/rutas/ruta-usuario-perfil/ruta-usuario-perfil.component.ts
+++ 03-angular/ejemplo/src/app/rutas/ruta-usuario-perfil/ruta-usuario-perfil.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {UserJphService} from "../../servicios/http/user-jph.service";
 import {UserJphInterface} from "../../servicios/http/interfaces/user-jph.interface";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {MatDialog} from "@angular/material/dialog";
+import {ModalEjemploComponent} from "../../componentes/modales/modal-ejemplo/modal-ejemplo.component";
 
 @Component({
   selector: 'app-ruta-usuario-perfil',
@@ -15,28 +17,93 @@ export class RutaUsuarioPerfilComponent implements OnInit {
   idUsuario = 0;
   usuarioActual?: UserJphInterface;
   formGroup?: FormGroup;
+  valorKnob = 5
+  items = [
+    {
+      label: 'Update', icon: 'pi pi-refresh', command: () =>{
+        console.log('Hola')
+      }
+    },
+    {
+      label: 'Setup', icon: 'pi pi-cog', routerLink: ['/setup']
+    }
+  ]
 
   constructor(
     private readonly activatedRoute:ActivatedRoute,
     private readonly userJphService: UserJphService,
-    private readonly formBuilder: FormBuilder
+    private readonly formBuilder: FormBuilder,
+    private readonly router: Router,
+    public dialog: MatDialog
   ) { }
+
+    abrirDialogo(){
+    const referenciaDialogo = this.dialog.open(
+      ModalEjemploComponent,
+      {
+        disableClose: true,
+        data: {
+          animal: 'panda'
+        }
+      }
+    );
+
+    const despuesCerrado$ = referenciaDialogo.afterClosed();
+    despuesCerrado$
+      .subscribe(
+        (datos) =>{
+          console.log(datos);
+        }
+      )
+    }
 
   ngOnInit(): void {
 
+    const parametroRuta$ = this.activatedRoute.params;
+
+    parametroRuta$
+      .subscribe(
+        {
+          next: (parametrosRuta) =>{
+            console.log(parametrosRuta);
+            this.idUsuario = + parametrosRuta['idUsuario'];
+            this.buscarUsuario(this.idUsuario);
+          }
+        }
+      )
+  }
+
+  buscarUsuario(id: number){
+    const buscarUsuarioPorId$ = this.userJphService.buscarUno(id);
+    buscarUsuarioPorId$
+      .subscribe(
+        {
+          next : (data) =>{
+            this.usuarioActual = data;
+            this.prepararFormulario();
+          },
+          error : (error) =>{
+            console.error(error)
+          }
+        }
+      )
+  }
+
+  prepararFormulario(){
     this.formGroup = this.formBuilder
       .group(
         {
           email: new FormControl(
             {
-              value: '',
+              value: this.usuarioActual? this.usuarioActual.email: '',
               disabled: false
             },
             [
               Validators.required, // min, max, minLegth, maxLength, email, pattern
               Validators.minLength(3)
             ]
-          )
+          ),
+          esAdministrador: new FormControl(true)
         }
       );
 
@@ -53,34 +120,43 @@ export class RutaUsuarioPerfilComponent implements OnInit {
         }
       }
     });
-
-    const parametroRuta$ = this.activatedRoute.params;
-
-    parametroRuta$
-      .subscribe(
-        {
-          next: (parametrosRuta) =>{
-            console.log(parametrosRuta);
-            this.idUsuario = + parametrosRuta['idUsuario'];
-            this.buscarUsuario(this.idUsuario);
-          }
-        }
-      )
   }
 
-
-  buscarUsuario(id: number){
-    const buscarUsuarioPorId$ = this.userJphService.buscarUno(id);
-    buscarUsuarioPorId$
-      .subscribe(
-        {
-          next : (data) =>{
-            this.usuarioActual = data;
-          },
-          error : (error) =>{
-            console.error(error)
-          }
+  prepararObjeto(){
+    if(this.formGroup){
+      const email = this.formGroup.get('email');
+      if (email){
+        return{
+          email: email.value
         }
-      )
+      }
+    }
+    return { email: ''}
+  }
+
+  actualizarUsuario(){
+    if(this.usuarioActual){
+      const valoresAActualizar = this.prepararObjeto();
+      const actualizar$ = this.userJphService
+        .actualizarPorId(
+          this.usuarioActual.id,
+          valoresAActualizar
+        );
+      actualizar$
+        .subscribe({
+          next:(datos) =>{
+            console.log({datos});
+            const url = ['/app','usuario'];
+            this.router.navigate(url);
+          },
+          error:(err) =>{
+            console.log({err});
+          }
+        })
+    }
+  }
+
+  guardar(){
+    console.log('GUARDAR')
   }
 }
